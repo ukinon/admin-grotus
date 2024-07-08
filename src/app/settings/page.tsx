b8d71651fe1da693b6a6769996dfc63d2e37a9aa
Arfiano Jordhy <arfianoj@gmail.com>
2024-07-08 08:32:54 +0700

    delete imports and update settings metadata

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,13 +2,10 @@ import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Image from "next/image";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import TableTwo from "@/components/Tables/TableTwo";
 import NutritionTable from "@/components/Tables/NutritionTable";
 
 export const metadata: Metadata = {
-  title: "Next.js Settings | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Settings page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+  title: "Settings | Grotus Admin",
 };
 
 const Settings = () => {
